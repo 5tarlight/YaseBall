652f2c0ca78cbd36695df5367bf093febc3c2751
YEAHx4 <developerstarlight@gmail.com>
2022-06-29 18:59:10 +0900

    init global style and page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Yase Ball</title>
+        <meta name="description" content="AI plays Number Baseball." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
